f2cc5afa1946ed28ecd88b564759a950f53a4c80
David Choy <38069452+davidchoy98@users.noreply.github.com>
2024-07-25 11:03:43 +0800

    Updated Page Root Layout

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import { websiteTitle } from "@/app/layout";
+import React, { FC, ReactElement } from "react";
+import Image from "next/image";
+
+export const NavBar: FC = (): ReactElement => {
+    return (
+        <div className="align-content-center position-sticky border border-dark-blue border-opacity-25 rounded mx-auto nav">
+            <div className="d-flex justify-content-between px-5">
+                <a href="#" className="navbar-brand text-decoration-none text-beige ms-4">
+                    <Image
+                        src={'/images/logo-transparent-beige.png'}
+                        alt={websiteTitle}
+                        width={60}
+                        height={60}
+                    />
+                    <span className="ms-4">{websiteTitle}</span>
+                </a>
+                <div>
+                    <a href="#about-me" className="btn text-beige mx-3 px-4 py-3">About Me</a>
+                    <a href="#skills" className="btn text-beige mx-3 px-4 py-3">Skills</a>
+                    <a href="#past-projects" className="btn text-beige mx-3 px-4 py-3">Showcase</a>
+                </div>
+                <a className="btn align-content-center text-beige px-5 call-action">Contact Me</a>
+            </div>
+        </div>
+    );
+}
